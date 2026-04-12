1c7cc91543e983b499c68cb30ec9ceb4cb2ea456
mreimundo <mreimundo@fi.uba.ar>
2026-04-12 17:29:38 -0300

    update de message_handler (no usos aun)

--- python/src/gateway/message_handler/message_handler.py
+++ python/src/gateway/message_handler/message_handler.py
@@ -5,14 +5,20 @@ class MessageHandler:
 
     def __init__(self):
         pass
-    
-    def serialize_data_message(self, message):
-        [fruit, amount] = message
-        return message_protocol.internal.serialize([fruit, amount])
 
-    def serialize_eof_message(self, message):
-        return message_protocol.internal.serialize([])
+    def serialize_data_message(self, message, client_id):
+        [fruit, amount] = message
+        return message_protocol.internal.serialize_data(client_id, fruit, amount)
+
+    def serialize_eof_message(self, message, client_id, sum_amount):
+        return message_protocol.internal.serialize_eof(
+            client_id, remaining=sum_amount - 1
+        )
 
     def deserialize_result_message(self, message):
-        fields = message_protocol.internal.deserialize(message)
-        return fields
+        msg = message_protocol.internal.deserialize(message)
+        client_id = msg.get("client_id")
+        fruit_top = msg.get("fruit_top")
+        if client_id is None or fruit_top is None:
+            return None
+        return (client_id, fruit_top)
