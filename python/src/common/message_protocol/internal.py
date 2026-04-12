2616c22c5273436473bd1512941484f036645568
mreimundo <mreimundo@fi.uba.ar>
2026-04-12 16:55:29 -0300

    cambio prot. int. para q sea por client id + tipo para identificar EOF

--- python/src/common/message_protocol/internal.py
+++ python/src/common/message_protocol/internal.py
@@ -1,5 +1,8 @@
 import json
 
+MSG_DATA = "data"
+MSG_EOF = "eof"
+
 
 def serialize(message):
     return json.dumps(message).encode("utf-8")
@@ -7,3 +10,28 @@ def serialize(message):
 
 def deserialize(message):
     return json.loads(message.decode("utf-8"))
+
+
+def make_data(client_id, fruit, amount):
+    return {"type": MSG_DATA, "client_id": client_id,
+            "fruit": fruit, "amount": amount}
+
+
+def make_eof(client_id, remaining):
+    return {"type": MSG_EOF, "client_id": client_id, "remaining": remaining}
+
+
+def is_data(msg):
+    return msg.get("type") == MSG_DATA
+
+
+def is_eof(msg):
+    return msg.get("type") == MSG_EOF
+
+
+def serialize_data(client_id, fruit, amount):
+    return serialize(make_data(client_id, fruit, amount))
+
+
+def serialize_eof(client_id, remaining):
+    return serialize(make_eof(client_id, remaining))
