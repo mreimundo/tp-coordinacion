b794d9aa3b1157c58d095c6bef1019509b091a16
mreimundo <mreimundo@fi.uba.ar>
2026-04-15 15:57:18 -0300

    fix de deserialize del msg handler

--- python/src/gateway/message_handler/message_handler.py
+++ python/src/gateway/message_handler/message_handler.py
@@ -6,22 +6,23 @@ class MessageHandler:
 
     def __init__(self):
         # generamos id cliente con optimismo de que no van a haber colisiones
-        self.client_id = str(uuid.uuid4())
-        pass
+        self._client_id = str(uuid.uuid4())
 
     def serialize_data_message(self, message):
         [fruit, amount] = message
-        return message_protocol.internal.serialize_data(self.client_id, fruit, amount)
+        return message_protocol.internal.serialize_data(self._client_id, fruit, amount)
 
     def serialize_eof_message(self, message):
         return message_protocol.internal.serialize_eof(
-            self.client_id
+            self._client_id
         )
 
     def deserialize_result_message(self, message):
         msg = message_protocol.internal.deserialize(message)
         client_id = msg.get("client_id")
         fruit_top = msg.get("fruit_top")
-        if client_id is None or fruit_top is None:
+
+        if client_id != self._client_id:
             return None
-        return (client_id, fruit_top)
+
+        return fruit_top
