44e15919f649d4100d106fef4943ad8a9ab01cf1
mreimundo <mreimundo@fi.uba.ar>
2026-04-15 12:54:54 -0300

    iteracion aggregation: input queue propia + conteo de eof

--- python/src/aggregation/main.py
+++ python/src/aggregation/main.py
@@ -1,4 +1,5 @@
 import os
+import signal
 import logging
 import bisect
 
@@ -17,54 +18,76 @@ TOP_SIZE = int(os.environ["TOP_SIZE"])
 class AggregationFilter:
 
     def __init__(self):
-        self.input_exchange = middleware.MessageMiddlewareExchangeRabbitMQ(
-            MOM_HOST, AGGREGATION_PREFIX, [f"{AGGREGATION_PREFIX}_{ID}"]
+        self.input_queue = middleware.MessageMiddlewareQueueRabbitMQ(
+            MOM_HOST, f"{AGGREGATION_PREFIX}_{ID}"
         )
         self.output_queue = middleware.MessageMiddlewareQueueRabbitMQ(
             MOM_HOST, OUTPUT_QUEUE
         )
-        self.fruit_top = []
+        # lo paso a diccionario para que sea por client_id
+        self.fruit_top = {}
+        self.eof_count = {}
 
-    def _process_data(self, fruit, amount):
+    def _process_data(self, client_id, fruit, amount):
         logging.info("Processing data message")
-        for i in range(len(self.fruit_top)):
-            if self.fruit_top[i].fruit == fruit:
-                self.fruit_top[i] = self.fruit_top[i] + fruit_item.FruitItem(
-                    fruit, amount
-                )
+        top = self.fruit_top.setdefault(client_id, [])
+        for i, fi in enumerate(top):
+            if fi.fruit == fruit:
+                top[i] = fi + fruit_item.FruitItem(fruit, amount)
                 return
-        bisect.insort(self.fruit_top, fruit_item.FruitItem(fruit, amount))
+        bisect.insort(top, fruit_item.FruitItem(fruit, amount))
 
-    def _process_eof(self):
+    def _process_eof(self, client_id):
         logging.info("Received EOF")
-        fruit_chunk = list(self.fruit_top[-TOP_SIZE:])
-        fruit_chunk.reverse()
-        fruit_top = list(
-            map(
-                lambda fruit_item: (fruit_item.fruit, fruit_item.amount),
-                fruit_chunk,
+        count = self.eof_count.get(client_id, 0) + 1
+        self.eof_count[client_id] = count
+        logging.info(f"[client={client_id}] EOF {count}/{SUM_AMOUNT}")
+
+        if count < SUM_AMOUNT:
+            return
+
+        # todos los Sum terminaron para este cliente: calculo un top parcial.
+        top = self.fruit_top.pop(client_id, [])
+        del self.eof_count[client_id]
+
+        partial = [
+            (fi.fruit, fi.amount)
+            for fi in reversed(top[-TOP_SIZE:])
+        ]
+        logging.info(f"[client={client_id}] Sending partial top ({len(partial)} items) to Join")
+        self.output_queue.send(
+            message_protocol.internal.serialize(
+                {"client_id": client_id, "fruit_top": partial}
             )
         )
-        self.output_queue.send(message_protocol.internal.serialize(fruit_top))
-        del self.fruit_top
 
     def process_messsage(self, message, ack, nack):
         logging.info("Process message")
-        fields = message_protocol.internal.deserialize(message)
-        if len(fields) == 2:
-            self._process_data(*fields)
+        msg = message_protocol.internal.deserialize(message)
+
+        if message_protocol.internal.is_data(msg):
+            self._process_data(msg["client_id"], msg["fruit"], msg["amount"])
+        elif message_protocol.internal.is_eof(msg):
+            self._process_eof(msg["client_id"])
         else:
-            self._process_eof()
+            logging.warning(f"Unknown message type: {msg.get('type')}")
+
         ack()
 
+    def _handle_sigterm(self, signum, frame):
+        logging.info("Received SIGTERM, stopping")
+        self._shutting_down = True
+        self.input_queue.stop_consuming()
+
     def start(self):
-        self.input_exchange.start_consuming(self.process_messsage)
+        signal.signal(signal.SIGTERM, self._handle_sigterm)
+        self.input_queue.start_consuming(self.process_message)
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    aggregation_filter = AggregationFilter()
-    aggregation_filter.start()
+    with AggregationFilter() as aggregation_filter:
+        aggregation_filter.start()
     return 0
 
 
